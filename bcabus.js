8a778c0bb2097aacde54a0f1208aba2729b621ec
Adam Papenhausen <debomb55555@gmail.com>
2016-11-23 00:49:30 -0500

    I stayed up to do favorites.

--- bcabus.js
+++ bcabus.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-    //Make this popup only appear once
-    Materialize.toast('Click here to choose your town', 5000)
+    var favTown;
+    $favoriteList = $(".favoriteList");
     
-    // Town Selector - Needs to
+    //Check if a favorite town has been set
+    if (localStorage.getItem("favTown") == null) {
+    Materialize.toast('<span style="margin: 0; cursor: pointer" class="toast">Click here to choose your town</span>', 5000)
+    }
+    //Update location with PHP (again later)
+    else {
+    favTown = localStorage.getItem("favTown");
+    $(".favoriteTown").html(favTown);
+    $favoriteList.show(100);
+    }
+    
+    $(".toast").click(function() {
+        $(window).scrollTo($("#town-select-link"),1000,"swing");
+    });
+    
+    // Town Selector - Replace with JSON
     $('input.autocomplete').autocomplete({
         data: {
             "Alpha": null,
@@ -14,21 +29,33 @@ $(document).ready(function() {
             "Golf": null
         }
     });
+    
+    //Use PHP to get location based on input again.
+    $townInput = $("#town-input")
+    $(".townSubmit").click(function() { $townInput.submit(); })
+    $townInput.submit(function() {
+        favTown = $townInput.val();
+        $(".favoriteTown").html(favTown);
+        localStorage.setItem("favTown",favTown);
+        $favoriteList.show(100);
+    })
+    
     //Works by #id of the list element
     $search = $("input[type='search']");
     $townItem = $(".townItem");
     $townHeader = $(".townList-header");
     $search.keyup(function() {
         $town = $search.val().toLowerCase();
-        console.log($town);
         if ($town !== "") {
             $townItem.not("[id*='" + $town + "']").hide(100);
             $(".collection-item[id*='" + $town + "']").show(100);
             $townHeader.hide(100);
+            $favoriteList.hide(100);
         }
         else {
             $townItem.show(100);
             $townHeader.show(100);
+            $favoriteList.show(100);
         }
     })
 })
