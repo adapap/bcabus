4c4220566d3c4bed39df5ac929c535e7c7750179
LaScalaLuke <lascala.luke@gmail.com>
2016-11-29 20:29:03 -0500

    Fixed bug where fav town disappered

--- bcabus.js
+++ bcabus.js
@@ -27,8 +27,21 @@ $(document).ready(function() {
         else {
             $favoriteList.show(100);
         }
-    
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
     //Works by #id of the list element
+    $hasfav = readCookie("favorite") !== null;
+    console.log($hasfav);
     $search = $("input[type='search']");
     $townItem = $(".townItem");
     $townHeader = $(".townList-header");
@@ -40,11 +53,15 @@ $(document).ready(function() {
             $townHeader.hide(100);
             $favoriteList.hide(100);
         }
+
         else {
+            console.log("hi");
             $townItem.show(100);
             $townHeader.show(100);
-            if (localStorage.getItem("favTown") !== null) { $favoriteList.show(100); };
+            if($hasfav) {
+                $favoriteList.show(100);
+            }
         }
     })
-})
+});
 
