c345855f0d9c0dbc6c65c3f8860cda2c03450aea
Luke LaScala <lascala.luke@gmail.com>
2017-03-28 17:50:10 -0400

    Working

--- bcabus.js
+++ bcabus.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var begin = dc.indexOf("; " + prefix);
         if (begin == -1) {
             begin = dc.indexOf(prefix);
-            if (begin != 0) return null;
+            if (begin !== 0) return null;
         }
         else
         {
@@ -22,28 +22,11 @@ $(document).ready(function() {
         return decodeURI(dc.substring(begin + prefix.length, end));
     }
     var favCookie = getCookie("favorite");
-    if (favCookie == null) {
+    if (favCookie === null) {
         Materialize.toast("Click the star to set a favorite town",6000);
     }
     var closeCookie = getCookie("madeby");
     $closeMsg.click(function() {
         $madeBy.slideUp(100);
-    })
-
-    //Works by #id of the list element
-    $search = $("input[type='search']");
-    $townItem = $(".townItem");
-    $townHeader = $(".townList-header");
-    $search.keyup(function() {
-        $town = $search.val().toLowerCase();
-        if ($town !== "") {
-            $townItem.not("[id*='" + $town + "']").hide(100);
-            $(".collection-item[id*='" + $town + "']").show(100);
-            $townHeader.hide(100);
-        }
-        else {
-            $townItem.show(100);
-            $townHeader.show(100);
-        }
     })
 })
